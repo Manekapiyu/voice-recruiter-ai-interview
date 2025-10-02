ff3e4d615512d197417b5df17a1cea714e4649fb
Manekapiyu <162763930+Manekapiyu@users.noreply.github.com>
2025-10-02 23:28:00 +0530

    connect supabase url

--- services/supabaseClient.js
+++ services/supabaseClient.js
@@ -1,4 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-// Create a single supabase client for interacting with your database
-const supabase = createClient('https://xyzcompany.supabase.co', 'publishable-or-anon-key')
+const supabaseUrl=process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey=process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+export const supabase = createClient(
+    supabaseUrl, 
+    supabaseAnonKey)
