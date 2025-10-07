67264c89011f8ac035ffc3fe8a6c5f6e046b9c06
Manekapiyu <162763930+Manekapiyu@users.noreply.github.com>
2025-10-07 23:35:08 +0530

    create new folder LatestInterviews

--- app/(main)/dashboard/_components/CreateOptions.jsx
+++ app/(main)/dashboard/_components/CreateOptions.jsx
@@ -1,15 +1,20 @@
 import React from "react";
-import {Video} from 'lucide-react';
+import { Video  } from "lucide-react";
+import { Phone } from "lucide-react";
 
 const CreateOptions = () => {
   return (
     <div className="grid grid-cols-2 gap-5">
-      <div className="bg-white boarder border-gray-200 rounded-lg" >
-       < Video className='p-3 text-primary bg-blue-50 rounded-lg h-14 w-14'/>
-<h2>Create New Interview</h2>
-<p>Quickly create and schedule your next AI interview session.</p>
+      <div className="bg-white boarder border-gray-200 rounded-lg">
+        <Video className="p-3 text-primary bg-blue-50 rounded-lg h-14 w-14" />
+        <h2>Create New Interview</h2>
+        <p>Quickly create and schedule your next AI interview session.</p>
       </div>
-      <div>Phone screening</div>
+      <div className="bg-white boarder border-gray-200 rounded-lg">
+        <Phone className="p-3 text-primary bg-blue-50 rounded-lg h-14 w-14" />
+        <h2>Create Phone Screening Call</h2>
+        <p>Quickly create and schedule your next Phone Screening Call session.</p>
+      </div>
     </div>
   );
 };
---
+++ app/(main)/dashboard/_components/LatestInterviews.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LatestInterviews = () => {
+  return (
+    <div>LatestInterviews</div>
+  )
+}
+
+export default LatestInterviews
