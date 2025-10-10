f17ac0c43bc421077f7f0f6b9def8df6a5dccfa1
Manekapiyu <162763930+Manekapiyu@users.noreply.github.com>
2025-10-10 09:42:21 +0530

    local updates before pull

--- app/(main)/dashboard/create-interview/_components/FormContainer.jsx
+++ app/(main)/dashboard/create-interview/_components/FormContainer.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
+import React from "react";
 
-function FormContainer () {
+function FormContainer() {
   return (
-    <div className='p-5 bg-gray-50'>
-      <h2 className=''>Job Position</h2>
-      <input placeholder="e.g. Full Stack Developer" className='mt-2'/>
-</div>
-<div>
-  <h2></h2>
-</div>
-
-
-  )
+    <div className="p-5 bg-gray-50">
+      <div>
+        <h2 className="">Job Position</h2>
+        <input placeholder="e.g. Full Stack Developer" className="mt-2" />
+      </div>
+      <div>
+        <h2></h2>
+      </div>
+    </div>
+  );
 }
 
-export default FormContainer
+export default FormContainer;
