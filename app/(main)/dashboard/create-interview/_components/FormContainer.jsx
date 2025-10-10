a1caec7daed1a1ae34b7a7a9ac4b7a773865c8a7
Manekapiyu <162763930+Manekapiyu@users.noreply.github.com>
2025-10-10 13:16:59 +0530

    add text ara and selector using shadcn

--- app/(main)/dashboard/create-interview/_components/FormContainer.jsx
+++ app/(main)/dashboard/create-interview/_components/FormContainer.jsx
@@ -1,14 +1,31 @@
 import React from "react";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
 
 function FormContainer() {
   return (
-    <div className="p-5 bg-gray-50">
+    <div className="p-5 bg-gray-50 rounded-lg shadow-sm">
       <div>
-        <h2 className="">Job Position</h2>
-        <input placeholder="e.g. Full Stack Developer" className="mt-2" />
+        <h2 className="text-sm font-medium">Job Position</h2>
+        <Input
+          placeholder="e.g. Full Stack Developer"
+          className="mt-2"
+        />
       </div>
-      <div>
-        <h2></h2>
+
+      <div className="mt-5">
+        <h2 className="text-sm font-medium">Job Description</h2>
+        <Textarea
+          placeholder="Enter detailed job description"
+          className="h-[200px] mt-2"
+        />
+      </div>
+      <div className="mt-5">
+        <h2 className="text-sm font-medium">Interview Duration</h2>
+        <Textarea
+
+          className="h-[200px] mt-2"
+        />
       </div>
     </div>
   );
